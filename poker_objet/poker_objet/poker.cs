8207b6414644f5f23238fd28b54211299612b794
Arigowin <david63210@gmail.com>
2014-08-28 12:38:16 +0200

    Creation de poker.cs
    Ajout du fichier contenant les classes

---
+++ poker_objet/poker_objet/poker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poker_objet
+{
+    class poker
+    {
+    }
+
+    class carte
+    { }
+
+    class jeux
+    { }
+
+}
